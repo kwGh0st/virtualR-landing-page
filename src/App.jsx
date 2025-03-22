5d646a68a21aa9b8ad6110c5ecefb00c3b1c35cd
Karol Programuje <karol341654@gmail.com>
2025-03-22 18:43:34 +0100

    Pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
 const App = () => {
   return (
     <>
@@ -11,6 +12,7 @@ const App = () => {
         <Hero />
         <Features />
         <Workflow />
+        <Pricing />
       </div>
     </>
   );
