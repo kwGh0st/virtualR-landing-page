ce6f63014cc40288227588a6acd4a95bbd17d6d5
Karol Programuje <karol341654@gmail.com>
2025-03-22 19:00:43 +0100

    testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Workflow from "./components/Workflow";
 import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 const App = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const App = () => {
         <Features />
         <Workflow />
         <Pricing />
+        <Testimonials />
       </div>
     </>
   );
